9647c3e7fc0dcb64103abe2951b413ea1d14cab4
Adrian Machado <machadoadriandev@gmail.com>
2022-10-23 00:20:05 -0300

    input style and send data

--- src/app/characters/components/search/search.component.ts
+++ src/app/characters/components/search/search.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
+  @Output() emitCharacter = new EventEmitter<string>();
+  nameCharac: string = '';
+  sendName: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  emitDataCharacter() {
+    this.sendName = this.nameCharac;
+    this.emitCharacter.emit(this.sendName);
   }
-
 }
